80fd8c117126fc52665bdd6ef2f671f58fa0fde6
Marc Stan <marcstan@live.com>
2020-11-20 21:47:53 +0100

    simplify keyvault injection

--- ExpirationScanner/Startup.cs
+++ ExpirationScanner/Startup.cs
@@ -3,8 +3,13 @@ using ExpirationScanner.Logic;
 using ExpirationScanner.Logic.Azure;
 using ExpirationScanner.Logic.Notification;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
 using System.Net.Http;
 
 [assembly: FunctionsStartup(typeof(ExpirationScanner.Startup))]
@@ -14,22 +19,8 @@ namespace ExpirationScanner
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            var keyVaultName = new ConfigurationBuilder()
-                .AddJsonFile("local.settings.json", optional: true)
-                .AddEnvironmentVariables()
-                .Build()["KeyVaultName"];
+            InjectKeyVaultIntoConfiguration(builder);
 
-            var configBuilder = new ConfigurationBuilder()
-                .AddJsonFile("local.settings.json", optional: true);
-
-            if (!string.IsNullOrEmpty(keyVaultName))
-                configBuilder.AddAzureKeyVault($"https://{keyVaultName}.vault.azure.net");
-
-            var config = configBuilder
-                    .AddEnvironmentVariables()
-                    .Build();
-
-            builder.Services.AddSingleton<IConfiguration>(config);
             builder.Services.Scan(scan =>
             {
                 scan.FromAssemblyOf<INotificationService>()
@@ -43,6 +34,31 @@ namespace ExpirationScanner
             builder.Services.AddSingleton<AzureManagementTokenProvider>();
             builder.Services.AddSingleton<KeyVaultExpiryChecker>();
             builder.Services.AddSingleton<AppRegistrationExpiryChecker>();
+        }
+
+        private void InjectKeyVaultIntoConfiguration(IFunctionsHostBuilder builder)
+        {
+            // https://stackoverflow.com/a/60349484
+            var serviceProvider = builder.Services.BuildServiceProvider();
+            var configurationRoot = serviceProvider.GetService<IConfiguration>();
+            var configurationBuilder = new ConfigurationBuilder();
+
+            if (configurationRoot is IConfigurationRoot)
+            {
+                configurationBuilder.AddConfiguration(configurationRoot);
+            }
+
+            var tokenProvider = new AzureServiceTokenProvider();
+            var kvClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(tokenProvider.KeyVaultTokenCallback));
+            var keyVaultName = configurationRoot["KeyVaultName"];
+            if (string.IsNullOrEmpty(keyVaultName))
+                throw new NotSupportedException("KeyVaultName must be set as an environment variable");
+
+            configurationBuilder.AddAzureKeyVault($"https://{keyVaultName}.vault.azure.net", kvClient, new DefaultKeyVaultSecretManager());
+
+            var configuration = configurationBuilder.Build();
+
+            builder.Services.Replace(ServiceDescriptor.Singleton(typeof(IConfiguration), configuration));
         }
     }
 }
